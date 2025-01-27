d4ba4af4d204c259848c2d9ea4efbb196097ca6a
1JzzQAQ <138588862+1JzzQAQ@users.noreply.github.com>
2025-01-27 16:36:15 +0800

    提交

--- src/stores/modules/user.ts
+++ src/stores/modules/user.ts
@@ -3,18 +3,35 @@ import { defineStore } from 'pinia'
 //引入接口
 import { reqLogin } from '@/api/user'
 //引入数据类型
-import type { loginForm } from '@/api/user/type'
+import type { loginForm, loginResponseData } from '@/api/user/type'
+import type { UserState } from './types/types'
+//引入操作本地存储的工具方法
+import { SET_TOKEN, GET_TOKEN } from '@/utils/token'
 //创建
 const useUserStore = defineStore('User', {
     //存储数据的地方
-    state: () => {
-        return {}
+    state: (): UserState => {
+        return {
+            token: GET_TOKEN(),//用户唯一标识token
+        }
     },
     //异步｜逻辑的地方
     actions: {
         //用户登录方法
-       async userLogin(data:loginForm) {
-            const result = await reqLogin(data)
+        async userLogin(data: loginForm) {
+
+            const result: loginResponseData = await reqLogin(data)
+            //登录请求成功200->token; 失败201
+            if (result.code == 200) {
+                //pinia仓库存储一下token
+                this.token = (result.data.token as string)
+                //本地持久化存储一份
+                SET_TOKEN(result.data.token as string)
+                //保证async返回一个成功的promise
+                return 'ok'
+            } else {
+                return Promise.reject(new Error(result.data.message))
+            }
         }
     },
     getters: {
--- src/views/login/indexLogin.vue
+++ src/views/login/indexLogin.vue
@@ -3,25 +3,17 @@
     <el-row>
       <el-col :span="12" :xs="0"></el-col>
       <el-col :span="12" :xs="24">
-        <el-form class="login_form">
+        <el-form class="login_form" :model="loginForm" :rules="rules" ref="loginForms">
           <h1>Hello</h1>
           <h2>Welcome to Guiguzhenxuan</h2>
-          <el-form-item>
+          <el-form-item prop="username">
             <el-input :prefix-icon="User" v-model="loginForm.username"></el-input>
           </el-form-item>
-          <el-form-item>
-            <el-input type="password" 
-            :prefix-icon="Lock" 
-            v-model="loginForm.password"
-            show-password
-            ></el-input>
+          <el-form-item prop="password">
+            <el-input type="password" :prefix-icon="Lock" v-model="loginForm.password" show-password></el-input>
           </el-form-item>
           <el-form-item>
-            <el-button class="login_btn" 
-            type="primary" 
-            size="default"
-            @click="login"
-            >登录</el-button>
+            <el-button :loading="loading" class="login_btn" type="primary" size="default" @click="login">登录</el-button>
           </el-form-item>
         </el-form>
       </el-col>
@@ -30,22 +22,62 @@
 </template>
 
 <script setup lang="ts">
-import {User,Lock} from '@element-plus/icons-vue';
-import { reactive } from 'vue';
+import { User, Lock } from '@element-plus/icons-vue';
+import { reactive, ref } from 'vue';
+import { useRouter } from 'vue-router'
+import { ElNotification } from 'element-plus'
 //引入用户相关的小仓库
 import useUserStore from '@/stores/modules/user'
+import { getTime } from '@/utils/time'
 const useStore = useUserStore()
+//获取el-form组件
+const loginForms = ref()
+//获取路由器
+const $router = useRouter()
+//定义变量控制按钮加载动画
+const loading = ref(false)
 //收集表单数据
-const loginForm = reactive({username:'admin',password:'111111'})
+const loginForm = reactive({ username: 'admin', password: '111111' })
 //登录按钮回调
-const login = ()=>{
+const login = async () => {
+  //保证表单项校验通过再发请求
+  await loginForms.value.validate()
+  loading.value = true
   //点击登录信息干什么？
   //通知仓库发登录请求
   //请求成功->跳转到home
   //请求失败->弹出失败信息
-  useStore.userLogin(loginForm)
+  try {
+    //可以书写.then的语法
+    await useStore.userLogin(loginForm)
+    //编程式导航跳转到展示数据的首页
+    $router.push('/')
+    //登录成功的提示信息
+    ElNotification({
+      type: 'success',
+      message: '欢迎回来',
+      title: `Hi,${getTime()}好`
+    })
+    loading.value = false
+  } catch (error) {
+    loading.value = false
+    //登录失败的提示信息
+    ElNotification({
+      type: 'error',
+      message: error.message
+    })
+  }
 }
-
+//定义表单校验需要的配置对象
+const rules = {
+  username: [
+    { required: true, message: '用户名不能为空', trigger: 'blur' },
+    { min: 3, max: 5, message: '用户名长度必须要3-5位', trigger: 'change' },
+  ],
+  password: [
+    { required: true, min: 6, max: 15, message: '密码长度必须要6-15位', trigger: 'change' }
+  ]
+}
 </script>
 
 <style scoped>
@@ -55,24 +87,28 @@ const login = ()=>{
   background: url('@/assets/images/background.jpg') no-repeat;
   background-size: cover;
 }
-.login_form{
+
+.login_form {
   position: relative;
   width: 80%;
   top: 25vh;
   background: url('@/assets/images/login_form.png') no-repeat;
   background-size: cover;
   padding: 10px 40px;
-  h1{
+
+  h1 {
     color: white;
     font-size: 40px;
-    border-bottom:0px ;
+    border-bottom: 0px;
   }
-  h2{
+
+  h2 {
     font-size: 20px;
     color: white;
     border-top: 0px;
   }
-  .login_btn{
+
+  .login_btn {
     width: 100%;
   }
 }
