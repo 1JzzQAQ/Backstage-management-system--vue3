03e772713c41fbd580ae26743fc72a534f9a7ab4
1JzzQAQ <138588862+1JzzQAQ@users.noreply.github.com>
2025-04-10 12:43:11 +0800

    提交

---
+++ src/views/screen/components/tourist/touristIndex.vue
@@ -0,0 +1,119 @@
+<template>
+    <div class="box">
+        <div class="top">
+            <p class="title">实时游客统计</p>
+            <p class="bg"></p>
+            <p class="rt">可预约总人数<span>99999</span>人</p>
+        </div>
+        <div class="number">
+            <span v-for="(item, index) in people" :key="index">{{ item }}</span>
+        </div>
+        <div class="charts" ref="charts">123</div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import * as echarts from 'echarts'
+import 'echarts-liquidfill'
+
+const people = ref('216908')
+const charts = ref()
+
+onMounted(() => {
+    const mycharts = echarts.init(charts.value)
+    mycharts.setOption({
+        title: {
+            text: '水球图'
+        },
+        //x|y轴组件
+        xAxis: {},
+        yAxis: {},
+        //系列:决定你展示什么样的图形图标
+        series: {
+            type: 'liquidFill',//系列
+            data: [0.6, 0.4, 0.2],//展示的数据
+            waveAnimation: true,//动画
+            animationDuration: 3,
+            animationDurationUpdate: 0,
+            radius: '80%',//半径
+            outline: {//外层边框颜色设置
+                show: true,
+                borderDistance: 8,
+                itemStyle: {
+                    color: 'skyblue',
+                    borderColor: '#294D99',
+                    borderWidth: 8,
+                    shadowBlur: 20,
+                    shadowColor: 'rgba(0, 0, 0, 0.25)'
+                }
+            },
+        },
+        //布局组件
+        grid: {
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0
+        }
+    })
+})
+
+
+</script>
+
+<style scoped>
+.box {
+    background: url(../../images/dataScreen-main-lb.png) no-repeat;
+    background-size: 100% 100%;
+    margin-top: 10px;
+
+    .top {
+        margin-left: 20px;
+
+        .title {
+            color: white;
+            font-size: 20px;
+        }
+
+        .bg {
+            background: url(../../images/dataScreen-title.png);
+            width: 68px;
+            height: 7px;
+            background-size: 100% 100%;
+        }
+
+        .rt {
+            float: right;
+            color: white;
+            font-size: 15px;
+
+            span {
+                color: orange;
+            }
+        }
+    }
+
+    .number {
+        margin-top: 30px;
+        display: flex;
+        padding: 10px;
+
+        span {
+            flex: 1;
+            height: 40 px;
+            text-align: center;
+            line-height: 40px;
+            background: url(../../images/total.png) no-repeat;
+            background-size: 100% 100%;
+            color: #29fcff;
+            font-size: 40px;
+        }
+    }
+
+    .charts {
+        width: 100%;
+        height: 270px;
+    }
+}
+</style>
--- src/views/screen/screenIndex.vue
+++ src/views/screen/screenIndex.vue
@@ -1,13 +1,122 @@
 <template>
-  <div>
-    <h1>shujudaping yiji</h1>
+  <div class="container">
+    <div class="screen" ref="screen">
+      <div class="top">
+        <Top />
+      </div>
+      <div class="bottom">
+        <div class="left">
+          <Tourist class="tourist"></Tourist>
+          <Sex class="sex"></Sex>
+          <Age class="age"></Age>
+        </div>
+        <div class="center"></div>
+        <div class="right"></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import Top from './components/top/topIndex.vue'
+import Tourist from './components/tourist/touristIndex.vue'
+import Age from './components/age/ageIndex.vue'
+import Sex from './components/sex/sexIndex.vue'
 
+const screen = ref()
+onMounted(() => {
+  screen.value.style.transform = `scale(${getScale()}) translate(-50%,-50%)`
+})
+
+function getScale(w = 1920, h = 1080) {
+  const ww = window.innerWidth / w
+  const wh = window.innerHeight / h
+  return ww < wh ? ww : wh
+}
+
+window.onresize = () => {
+  screen.value.style.transform = `scale(${getScale()}) translate(-50%,-50%)`
+}
 </script>
 
 <style scoped>
+.container {
+  width: 100vw;
+  height: 100vh;
+  background: url(./images/bg.png) no-repeat;
+  background-size: cover;
 
+  .screen {
+    position: fixed;
+    width: 1920px;
+    height: 1080px;
+    left: 50%;
+    top: 50%;
+    transform-origin: left top;
+
+    .top {
+      width: 100%;
+      height: 40px;
+    }
+
+    .bottom {
+      display: flex;
+
+      .right {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        margin-left: 40px;
+
+        .rank {
+          flex: 1.5;
+        }
+
+        .year {
+          flex: 1;
+
+        }
+
+        .count {
+          flex: 1;
+        }
+      }
+
+      .left {
+        flex: 1;
+        height: 1040px;
+        display: flex;
+        flex-direction: column;
+
+        .tourist {
+          flex: 1.2;
+        }
+
+        .sex {
+          flex: 1;
+
+        }
+
+        .age {
+          flex: 1;
+        }
+      }
+
+      .center {
+        flex: 1.5;
+        display: flex;
+        flex-direction: column;
+
+        .map {
+          flex: 4;
+        }
+
+        .line {
+          flex: 1;
+        }
+      }
+    }
+  }
+}
 </style>
